775ce7acf3adfd61a364fd914d2e0872db26f391
ashryaagr <ashryaagr@gmail.com>
2020-10-27 18:57:41 +0530

    Jagged array type printing

--- printTypeExpression.c
+++ printTypeExpression.c
@@ -61,6 +61,22 @@ void printTypeExpression(int type, TypeExpression typeExpr)
     }
     case JAGGED_ARRAY:
     {
+        JaggedArray jaggArr = typeExpr.jaggedType;
+        printf("jaggedArray, dimensions=%d, ", jaggArr.is2D ? 2 : 3);
+        printf("range_R1 = (%d,%d), range_R2 = (", jaggArr.r1Low, jaggArr.r1High);
+        int r1Len = jaggArr.r1High - jaggArr.r1Low + 1;
+        for(int i=0; i<r1Len; i++) {
+            if (jaggArr.is2D) {
+                printf(" %d,", jaggArr.type.twod_array.size[i]);
+            } else {
+                printf("%d[", jaggArr.type.threed_array.sizeR2[i]);
+                for(int j=0; j<jaggArr.type.threed_array.sizeR2[i]; j++){
+                    printf("%d,", jaggArr.type.threed_array.size[i][j]);
+                }
+                printf("], ");
+            }
+        }
+        printf("), basicElementType = integer");
         break;
     }
     }
